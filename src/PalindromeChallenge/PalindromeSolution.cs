2f17cfbb6d432cc1471bfa15ff014930ed25b879
Carlos Junior <carlos.junior@hotmail.com.br>
2024-12-06 22:55:13 -0300

    Solution

---
+++ src/PalindromeChallenge/PalindromeSolution.cs
@@ -0,0 +1,21 @@
+﻿namespace PalindromeChallenge;
+
+public class PalindromeSolution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+
+        var original = x;
+        var reversedNumber = 0;
+        while (x != 0)
+        {
+            var rest = x % 10;
+            reversedNumber = reversedNumber * 10 + rest;
+            x = (x / 10);
+        }
+
+        return original == reversedNumber;
+    }
+}
---
+++ tests/UnitTests/PalindromeSolutionTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using PalindromeChallenge;
+using Xunit;
+
+namespace UnitTests;
+
+public class PalindromeSolutionTests
+{
+    [Theory]
+    [InlineData(11)]
+    [InlineData(121)]
+    [InlineData(222)]
+    [InlineData(1331)]
+    public void Must_return_true_for_palindromes_numbers(int number)
+    {
+        ///Arrange
+        var palindromeValidation = new PalindromeSolution();
+
+        ///Act
+        var isPalindrome = palindromeValidation.IsPalindrome(number);
+
+        ///Assert
+        isPalindrome.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(10)]
+    [InlineData(201)]
+    [InlineData(12341)]
+    public void Must_return_false_for_non_palindromes_numbers(int number)
+    {
+        ///Arrange
+        var palindromeValidation = new PalindromeSolution();
+
+        ///Act
+        var isPalindrome = palindromeValidation.IsPalindrome(number);
+
+        ///Assert
+        isPalindrome.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(-100)]
+    public void Must_return_false_for_negative_numbers(int number)
+    {
+        ///Arrange
+        var palindromeValidation = new PalindromeSolution();
+
+        ///Act
+        var isPalindrome = palindromeValidation.IsPalindrome(number);
+
+        ///Assert
+        isPalindrome.Should().BeFalse();
+    }
+}
